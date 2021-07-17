6757f397f7ce51090c599fd6205dee6585e7fd4c
Vincent Vic <vcoding@dingtalk.com>
2021-07-17 14:33:24 +0800

    快照定时采集定时任务合并到统计定时任务

--- src/main/java/cn/bugio/wiki/job/EbookStatisticsJob.java
+++ src/main/java/cn/bugio/wiki/job/EbookStatisticsJob.java
@@ -1,6 +1,7 @@
 package cn.bugio.wiki.job;
 
 import cn.bugio.wiki.service.EbookService;
+import cn.bugio.wiki.service.EbookSnapshotService;
 import cn.bugio.wiki.util.SnowFlake;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
@@ -21,16 +22,19 @@ public class EbookStatisticsJob {
 
     private final EbookService ebookService;
 
+    private final EbookSnapshotService ebookSnapshotService;
+
     private final SnowFlake snowFlake;
     @Autowired
-    public EbookStatisticsJob(EbookService ebookService, SnowFlake snowFlake) {
+    public EbookStatisticsJob(EbookService ebookService, EbookSnapshotService ebookSnapshotService, SnowFlake snowFlake) {
         this.ebookService = ebookService;
+        this.ebookSnapshotService = ebookSnapshotService;
         this.snowFlake = snowFlake;
     }
 
 
     /**
-     * 凌晨四点更新，减少服务器繁忙时开销
+     * 1,6,10,15,19,23更新
      */
     @Scheduled(cron = "0 0 1,6,10,15,19,23 * * ?")
     public void statisticsJob(){
@@ -40,5 +44,18 @@ public class EbookStatisticsJob {
         long start = System.currentTimeMillis();
         int count = ebookService.updateEbookStatistics();
         log.info("定时统计电子书数据结束,影响{}条数据，耗时:{} 毫秒",count, System.currentTimeMillis()-start);
+    }
+
+    /**
+     * 1,6,10,15,19,23的30分钟更新
+     */
+    @Scheduled(cron = "0 30 1,6,10,15,19,23 * * ?")
+    public void snapshotJob(){
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
+        log.info("定时更新快照开始");
+        long start = System.currentTimeMillis();
+        int count = ebookSnapshotService.genEbookSnapshot();
+        log.info("定时更新快照结束,影响{}条数据，耗时:{} 毫秒",count, System.currentTimeMillis()-start);
     }
 }
