ec2ebd82c4ce331c886c34ef6262d502ab20277a
Vincent Vic <vcoding@dingtalk.com>
2021-07-17 13:58:16 +0800

    快照表操作生成

---
+++ src/main/java/cn/bugio/wiki/dao/EbookSnapshotMapper.java
@@ -0,0 +1,7 @@
+package cn.bugio.wiki.dao;
+
+import cn.bugio.wiki.domain.entity.EbookSnapshot;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface EbookSnapshotMapper extends Mapper<EbookSnapshot> {
+}
---
+++ src/main/java/cn/bugio/wiki/domain/entity/EbookSnapshot.java
@@ -0,0 +1,55 @@
+package cn.bugio.wiki.domain.entity;
+
+import java.util.Date;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Table(name = "ebook_snapshot")
+public class EbookSnapshot {
+    /**
+     * id
+     */
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Long id;
+
+    /**
+     * 电子书id
+     */
+    @Column(name = "ebook_id")
+    private Long ebookId;
+
+    /**
+     * 快照日期
+     */
+    private Date date;
+
+    /**
+     * 阅读数
+     */
+    @Column(name = "view_count")
+    private Integer viewCount;
+
+    /**
+     * 点赞数
+     */
+    @Column(name = "vote_count")
+    private Integer voteCount;
+
+    /**
+     * 阅读增长
+     */
+    @Column(name = "view_increase")
+    private Integer viewIncrease;
+
+    /**
+     * 点赞增长
+     */
+    @Column(name = "vote_increase")
+    private Integer voteIncrease;
+}
